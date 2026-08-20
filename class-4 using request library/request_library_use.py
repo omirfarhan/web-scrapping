4a73cdffceed175a2bcdd1c961d8ce480fcb29e0
omirfarhan <omirfarhan1@gmail.com>
2026-08-20 16:19:31 +0600

    web scraping end project

--- class-4 using request library/request_library_use.py
+++ class-4 using request library/request_library_use.py
@@ -3,8 +3,14 @@ import requests
 html_text = requests.get('https://books.toscrape.com').text
 soup = BeautifulSoup(html_text, 'lxml')
 
+domainlink='https://books.toscrape.com/'
+
 # product_list = soup.find_all('ol', class_ ='row')
 # print(product_list)
+
+# print('put your searching book.... ')
+# unfamilier_book = input('>')
+# print(f'Filtering out book {unfamilier_book}')
 
 first_book=soup.find_all('li',class_ = 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
 for books in first_book:
@@ -13,12 +19,30 @@ for books in first_book:
      product_price = product_list_price.find('p').text
      rating_tag = books.find('p',class_='star-rating')
      product_rating = rating_tag.get('class')[-1]
+     product_description = books.find('div', class_ = 'image_container')
+     product_link_des = product_description.find('a')['href']
+     image_link = product_description.find('img')['src']
 
-     print(f'''
-Book name = {title}
-product price = {product_price}
-product rating = {product_rating}
-     ''')
+     print(f"Book name = {title.strip()}")
+     print(f"product price = {product_price.strip()}")
+     print(f"product rating = {product_rating.strip()}")
+     print(f"Product description link = {product_link_des}")
+     print(f"Product image link = {domainlink+image_link}")
+     print(' ')
+
+     # if unfamilier_book not in title:
+          
+
+     
+
+
+     
+
+#      print(f'''
+# Book name = {title}
+# product price = {product_price}
+# product rating = {product_rating}
+#      ''')
 
 
 # #ekhane ekta product er data gula dekhacche 
